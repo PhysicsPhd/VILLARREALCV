9ccd4a20be1de8e7c5fb9b3db5789df080b711a4
Julian Villarreal <javillarrealmurua@gmail.com>
2023-09-23 23:13:09 -0300

    entendiedo fetch

--- js/fetchimginfo.js
+++ js/fetchimginfo.js
@@ -1,5 +1,15 @@
-const numimagenes=3;
-
-fetch('https://drive.google.com/file/d/15pJi5cTI39lS1c_SUkIV9rWjSuXjbLoR/view?usp=sharing')
-        .then(response => response.json())
-        .then(json => console.log(json))
+/*
+async function loadNames() {
+        const response = await fetch('https://api.jsonbin.io/v3/b/650f8a2712a5d3765982480e');
+        const names = await response.json();
+      
+        console.log(names);
+        console.log(names.record.srctext); 
+        // logs [{ name: 'Joker'}, { name: 'Batman' }]
+      }
+      
+      loadNames();
+*/
+      fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then((response) => response.json())
+      .then((json) => console.log(json));
